44a4d3459cefe7dc5f3f167be52da68a2541fa1c
TobiaszLos <tobiasz1512@gmail.com>
2022-12-01 23:18:30 +0100

    selected task store

--- src/model/FullList.ts
+++ src/model/FullList.ts
@@ -3,16 +3,21 @@ import { TaskItem } from './TaskItem'
 
 interface List {
   list: ListItem[]
-  save(): void
+  selectedItem: string
+  saveList(): void
   clearList(): void
   addItem(item: ListItem): void
   removeItem(id: string): void
+  saveSelectedItem(id: string): void
+  setSelectedItem(id: string): void
 }
 
 type ListItemObj = { _id: string; _item: string; _tasks: TaskItem[] }
 
 export class FullList implements List {
   static instance: FullList = new FullList()
+
+  _selectedItem: string = localStorage.getItem('mySelectedItem')!
 
   constructor (private _list: ListItem[] = []) {}
 
@@ -32,23 +37,41 @@ export class FullList implements List {
     return this._list
   }
 
-  save () {
+  get selectedItem (): string {
+    return this._selectedItem
+  }
+
+  saveList () {
     localStorage.setItem('myTodoList', JSON.stringify(this._list))
+  }
+
+  saveSelectedItem (id: string) {
+    localStorage.setItem('mySelectedItem', id)
   }
 
   clearList () {
     this._list = []
-    this.save()
+    this.saveList()
   }
 
   addItem (item: ListItem) {
-    console.log('added ITEM', item, this.list)
     this._list.push(item)
-    this.save()
+    this.saveList()
   }
 
   removeItem (id: string) {
     this._list.filter(item => item.id !== id)
-    this.save()
+    this.saveList()
+  }
+
+  setSelectedItem (id: string) {
+    this._selectedItem = id
+    this.saveSelectedItem(id)
+  }
+
+  addTaskToList (task: TaskItem) {
+    const currentList = this._list.find(item => item.id === this._selectedItem)
+    currentList?.tasks.push(task)
+    this.saveList()
   }
 }
