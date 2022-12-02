a7279d60ac607afcdcca1a00ac170c5a80208820
TobiaszLos <tobiasz1512@gmail.com>
2022-12-02 20:50:37 +0100

    uc

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import { TaskItem } from './model/TaskItem'
 import { ListTemplate } from './templates/ListTemplate'
 
 import { v4 as uuidv4 } from 'uuid'
+import { TaskTemplate } from './templates/TaskTemplate'
 
 const initApp = (): void => {
   const fullList = FullList.instance // local storage full list
   const templateList = ListTemplate.instance
+  const templateTasks = TaskTemplate.instante
 
+  // Add new item to list of todos
   const addListForm = document.querySelector(
     '[data-todo-form]'
   ) as HTMLFormElement
@@ -34,9 +37,14 @@ const initApp = (): void => {
   // render template base on local stroage data
   templateList.render(fullList)
 
+
+
+
   const taskItemLog = new TaskItem()
   const listItemLog = new ListItem()
-  console.log({ taskItemLog, listItemLog, fullList })
+  console.log({ taskItemLog, listItemLog, fullList }, 'LOG Z INDEX><JS')
+
+  console.log({ taskTemplate: new TaskTemplate() })
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -1,31 +1,34 @@
-import { FullList } from "../model/FullList"
+import { FullList } from '../model/FullList'
+import { TaskItem } from '../model/TaskItem'
 
 interface DOMList {
   ul: HTMLUListElement
   clear(): void
   render(fullList: FullList): void
+  // selectedItem: string
 }
 
 export class ListTemplate implements DOMList {
   ul: HTMLUListElement
+  // selectedItem: string
 
   static instance: ListTemplate = new ListTemplate()
 
-  
   constructor () {
     this.ul = document.querySelector(
       '[data-todo-list-container]'
     ) as HTMLUListElement
+
+    // this.selectedItem = localStorage.getItem('mySelectedItem')!
   }
 
   clear () {
     this.ul.innerHTML = ''
   }
 
-
-  render(fullList: FullList) {
+  render (fullList: FullList) {
     this.clear()
-
+    console.log({ ...fullList }, 'LOG Z render(fullList: FullList)')
     fullList.list.forEach(item => {
       const LiElement = document.createElement('li')
       LiElement.classList.add('list-name')
@@ -35,12 +38,19 @@ export class ListTemplate implements DOMList {
         `
       //data-list-todo-id
       LiElement.dataset.listTodoId = item.id
-    
-      // if (item.id === selectedTodoListTask) {
-      //   LiElement.setAttribute('active-list', '')
-      // }
+
+      LiElement.addEventListener('click', () => {
+        fullList.setItemAndCurrentList(item.id)
+        this.render(fullList)
+
+        // fullList.addTaskToList(new TaskItem('123fff','sniadanie', false))
+
+      })
+
+      if (item.id === fullList._selectedItem) {
+        LiElement.setAttribute('active-list', '')
+      }
       this.ul.appendChild(LiElement)
     })
   }
-
 }
