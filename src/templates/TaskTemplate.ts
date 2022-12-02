c04f3637d34500166ca116b8c5d3fdf28c2d0cfc
TobiaszLos <tobiasz1512@gmail.com>
2022-12-02 20:49:43 +0100

    Add task template

---
+++ src/templates/TaskTemplate.ts
@@ -0,0 +1,47 @@
+import { TaskItem } from '../model/TaskItem'
+
+export class TaskTemplate {
+  private ul: HTMLUListElement
+
+  static instante: TaskTemplate = new TaskTemplate()
+
+  constructor () {
+    this.ul = document.querySelector(
+      '[data-tasks-container]'
+    ) as HTMLUListElement
+  }
+
+  render (tasksList: TaskItem[]) {
+    tasksList.forEach((task, id) => {
+      const inputEl = document.createElement('input')
+      inputEl.type = 'checkbox'
+      inputEl.name = ''
+      inputEl.setAttribute('id', `${id}-${task.name}`)
+      inputEl.setAttribute('class', 'todo-task-checkbox')
+      inputEl.checked = task.completed
+
+      const labelEl = document.createElement('label')
+      labelEl.setAttribute('class', 'checkbox-label-container')
+      labelEl.setAttribute('for', `${id}-${task.name}`)
+
+      labelEl.onclick = () => {
+        task.completed = !task.completed
+        // saveToLocalStorage(todosList)
+      }
+
+      const customSpan = document.createElement('span')
+      customSpan.setAttribute('class', 'custom-checkbox')
+
+      const span = document.createElement('span')
+      span.setAttribute('class', 'task-text')
+      span.innerText = task.name
+
+      labelEl.append(customSpan, span)
+
+      const containerElement = document.createElement('div')
+      containerElement.append(inputEl, labelEl)
+
+      this.ul.append(containerElement)
+    })
+  }
+}
